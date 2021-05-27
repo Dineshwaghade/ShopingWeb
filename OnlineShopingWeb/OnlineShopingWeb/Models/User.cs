f7902f44012ee1be79ee2bfd956dea0e85553bcf
Dinesh waghade <dineshwaghade12@gmail.com>
2021-05-27 13:09:25 +0530

    //add database connection & create user module

---
+++ OnlineShopingWeb/OnlineShopingWeb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopingWeb.Models
+{
+    public class User
+    {
+        public int User_id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Contact { get; set; }
+        public string Registration_id { get; set; }
+    }
+}

7040973b4f8e93134f369a20c9b37ca90f230afe
Dinesh waghade <dineshwaghade12@gmail.com>
2021-05-27 14:40:39 +0530

    //admin layout

---
+++ OnlineShopingWeb/OnlineShopingWeb/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShopingWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopingWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var data = db.Users.ToList();
+            return View(data);
+        }
+    }
+}

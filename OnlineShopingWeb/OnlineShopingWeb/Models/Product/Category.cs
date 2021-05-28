b8dc7f999cbe1e81a344e65ba97cc6d1d9364af1
Dinesh waghade <dineshwaghade12@gmail.com>
2021-05-28 16:47:08 +0530

    //category

---
+++ OnlineShopingWeb/OnlineShopingWeb/Models/Product/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopingWeb.Models.Product
+{
+    public class Category
+    {
+        [Key]
+        public int Category_id { get; set; }
+        [Required]
+        public string Category_Name { get; set; }
+        public virtual ICollection<SubCategory> Subcategories { get; set; }
+
+    }
+}

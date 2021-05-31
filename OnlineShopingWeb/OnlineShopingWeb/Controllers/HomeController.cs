8066fc363b950f8797279bd5db81a6120bbddb45
Dinesh waghade <dineshwaghade12@gmail.com>
2021-06-01 01:03:10 +0530

    // product details home

--- OnlineShopingWeb/OnlineShopingWeb/Controllers/HomeController.cs
+++ OnlineShopingWeb/OnlineShopingWeb/Controllers/HomeController.cs
@@ -16,5 +16,26 @@ namespace OnlineShopingWeb.Controllers
             var list = db.Products.ToList();
             return View(list);
         }
+        [HttpGet]
+        public ActionResult ProductDetails(int id)
+        {
+            //List<SubCategory> lst = db.SubCategories.ToList();
+            //ViewBag.SCList = new SelectList(lst, "SubCategory_id", "SubCategory_Name");
+
+            var data = db.Products.Find(id);
+            //if (data.Product_Image != null)
+            //{
+            //    TempData["UserImage"] = data.Product_Image;
+            //    TempData.Keep();
+            //}
+            if (data.Product_Image == null || data.Product_Image == string.Empty)
+            {
+                data.Product_Image = "~/Images/img-not-found.png";
+            }
+
+            return View(data);
+        }
+
     }
+
 }
